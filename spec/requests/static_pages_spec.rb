235e7420cb8c33eef22c5ce53c3d42081ca86012
denothsk <denothsk@gmail.com>
2013-10-16 21:21:42 +0300

    Finish layout and routes a part

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,12 @@ let (:base_title) {"Rail App"}
   describe "Home page" do
 
     it "should have the content 'Rail App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Rail App')
     end
 
      it "should have the right title 'Rail App | Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title} | Home")
     end
   end
@@ -20,12 +20,12 @@ let (:base_title) {"Rail App"}
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title 'Rail App | Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end 
   end
@@ -34,12 +34,12 @@ let (:base_title) {"Rail App"}
  describe "About Us" do
 
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About')
     end
 
     it "should have the right title 'Rail App | About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About")
     end 
   end
@@ -48,12 +48,12 @@ let (:base_title) {"Rail App"}
  describe "Contact" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the right title 'Rail App | Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end 
   end
